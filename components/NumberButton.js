fbe786e36af96a571d41c495c1066f8a55ed0341
hanifaarrumaisha <9handr4_Ud1n>
2020-07-30 14:22:56 +0700

    add button component

--- components/NumberButton.js
+++ components/NumberButton.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { Text, StyleSheet} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const NumberButton = () => {
-    
+export default KeypadButton = ({textButton, onPressAction}) => {
+    <TouchableOpacity 
+        accessibilityRole={'button'}
+        onPress={onPressAction}
+        style={styles.container}>
+            <Text style={styles.text}>{textButton}</Text>
+        
+    </TouchableOpacity>
 }
+
+styles = StyleSheet.create({
+    container:{
+        flexWrap: 'wrap',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    text:{
+        fontSize: 18,
+    }
+})
