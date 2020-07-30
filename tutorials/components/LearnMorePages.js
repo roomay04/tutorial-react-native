799fb80079ccecb8779f3edbf8ca04230a4f3aa8
hanifaarrumaisha <9handr4_Ud1n>
2020-07-30 12:05:13 +0700

    gunain learn more page sendiri

---
+++ tutorials/components/LearnMorePages.js
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+import type {Node} from 'react';
+import openURLInBrowser from 'react-native/Libraries/Core/Devtools/openURLInBrowser';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const Colors = {
+    primary: '#1292B4',
+    white: '#FFF',
+    lighter: '#F3F3F3',
+    light: '#DAE1E7',
+    dark: '#444',
+    black: '#000',
+  };
+
+const links = [
+  {
+    id: 1,
+    title: 'The Basics',
+    link: 'https://reactnative.dev/docs/tutorial',
+    description: 'Explains a Hello World for React Native.',
+    navigationPage: 'FlexTutorial',
+  },
+  {
+    id: 2,
+    title: 'Style',
+    link: 'https://reactnative.dev/docs/style',
+    description:
+      'Covers how to use the prop named style which controls the visuals.',
+    navigationPage: 'FlexTutorial',
+  },
+  {
+    id: 3,
+    title: 'Layout',
+    link: 'https://reactnative.dev/docs/flexbox',
+    description: 'React Native uses flexbox for layout, learn how it works.',
+    navigationPage: 'FlexTutorial',
+  },
+  {
+    id: 4,
+    title: 'Components',
+    link: 'https://reactnative.dev/docs/components-and-apis',
+    description: 'The full list of components and APIs inside React Native.',
+    navigationPage: 'FlexTutorial',
+  },
+  {
+    id: 5,
+    title: 'Navigation',
+    link: 'https://reactnative.dev/docs/navigation',
+    description:
+      'How to handle moving between screens inside your application.',
+    navigationPage: 'FlexTutorial',
+  },
+  {
+    id: 6,
+    title: 'Networking',
+    link: 'https://reactnative.dev/docs/network',
+    description: 'How to use the Fetch API in React Native.',
+    navigationPage: 'FlexTutorial',
+  },
+  {
+    id: 7,
+    title: 'Help',
+    link: 'https://reactnative.dev/help',
+    description:
+      'Need more help? There are many other React Native developers who may have the answer.',
+    navigationPage: 'FlexTutorial',
+  },
+  {
+    id: 8,
+    title: 'Follow us on Twitter',
+    link: 'https://twitter.com/reactnative',
+    description:
+      'Stay in touch with the community, join in on Q&As and more by following React Native on Twitter.',
+    navigationPage: 'FlexTutorial',
+  },
+];
+
+const LinkList = ({navigation}): Node => (
+  <View style={styles.container}>
+    {links.map(({id, title, link, description, navigationPage}) => {
+      return (
+        <React.Fragment key={id}>
+          <View style={styles.separator} />
+          <TouchableOpacity
+            accessibilityRole={'button'}
+            onPress={() => 
+              navigation.navigate(navigationPage, { name: 'Jane' })
+            }
+            style={styles.linkContainer}>
+            <Text style={styles.link}>{title}</Text>
+            <Text style={styles.description}>{description}</Text>
+          </TouchableOpacity>
+        </React.Fragment>
+      );
+    })}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  linkContainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  link: {
+    flex: 2,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.primary,
+  },
+  description: {
+    flex: 3,
+    paddingVertical: 16,
+    fontWeight: '400',
+    fontSize: 18,
+    color: Colors.dark,
+  },
+  separator: {
+    backgroundColor: Colors.light,
+    height: 1,
+  },
+});
+
+export default LinkList;
