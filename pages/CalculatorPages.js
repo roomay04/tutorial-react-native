2c88136ec3d458de2988b1f2d478ac058da45d69
hanifaarrumaisha <9handr4_Ud1n>
2020-08-03 11:42:55 +0700

    tampil di result text

--- pages/CalculatorPages.js
+++ pages/CalculatorPages.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import Colors from '../components/Colors'
 import KeypadButtons from '../components/KeypadButtons';
 
@@ -9,14 +9,11 @@ export default CalculatorPages = () => {
     const onPress = function (text) {
         setResultText(text);
     }
-
-    useEffect(() => {
-        alert(resultText);
-    })
-
+    
     return (
         <View style={styles.container}>
             <View style={styles.contResult}> 
+                <Text style={styles.resultText}>{resultText}</Text>
             </View>
 
             <View style={styles.contButtons}>
@@ -35,10 +32,14 @@ const styles = StyleSheet.create({
         flex: 3,
         backgroundColor: Colors.light,
         justifyContent: 'flex-end',
+        alignItems: 'flex-end',
     },
     contButtons: {
         flex: 7,
         alignItems: 'stretch',
         backgroundColor: Colors.lighter,
+    },
+    resultText:{
+        fontSize: 50,
     }
 })
